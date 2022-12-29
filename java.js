1afeb6faa4455f668d523a2009154ec52e2a7e22
Aldo León <aldo.leon.h@hotmail.com>
2022-12-29 11:57:25 -0600

    una pagina extra que se agregara a la pagina web del segundo repo.

---
+++ java.js
@@ -0,0 +1,44 @@
+alert();
+
+
+
+
+
+
+
+
+
+
+
+
+// let numero = document.getElementById("num");
+// let resultado = document.getElementById("res");
+// let a = prompt("Ingresa el número A:");
+// let b = prompt("Ingresa el número B:");
+// let c = prompt("Ingresa el número C:");
+// numero.textContent=" Los numeros son A=" + a + ", B=" + b + " y C=" + c;
+
+// if(a>b && a>c){
+//     if(b>c){
+//         resultado.textContent= `El orden correcto es A B C, con valores: ${a}, ${b} y ${c}`
+//     }
+//     else{
+//         resultado.textContent= `El orden correcto es A C B, con valores: ${a}, ${c} y ${b}`
+//     }
+// }
+// else if(b>a && b>c){
+//     if(a>c){
+//         resultado.textContent= `El orden correcto es B A C, con valores: ${b}, ${a} y ${c}`
+//     }
+//     else{
+//         resultado.textContent= `El orden correcto es B C A, con valores: ${b}, ${c} y ${a}`
+//     }
+// }
+// else if(c>a && c>b){
+//     if(a>b){
+//         resultado.textContent= `El orden correcto es C A B, con valores: ${c}, ${a} y ${b}`
+//     }
+//     else{
+//         resultado.textContent= `El orden correcto es C B A, con valores: ${c}, ${b} y ${a}`
+//     }
+// }
